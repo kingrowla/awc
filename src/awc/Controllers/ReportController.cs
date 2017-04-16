2a0c20794c4747e55bdd107059c18595af75ff65
Anson Terrell <kingrowla@outlook.com>
2017-04-15 23:50:37 -0500

    css updates to reports view, minor updates to controller, favicon change

--- src/awc/Controllers/ReportController.cs
+++ src/awc/Controllers/ReportController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using awc.Entities;
+using awc.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -20,8 +21,93 @@ namespace awc.Controllers
         [Route("Index")]
         public IActionResult Index()
         {
-            var customers = db.Customer.OrderByDescending(u => u.FirstName).Take(5);
-            return View(customers.ToList());
+            //var customers = db.Customer.OrderByDescending(u => u.FirstName).Take(5);
+            //return View(customers.ToList());
+            //ElmahError = grp.OrderBy(s => s.ElmahError.TimeUtc).FirstOrDefault();
+            List<ReportAttributes> model = new List<ReportAttributes>();
+                                  var innerJoinQuery = (from c in db.Customer
+                                  join s in db.SalesOrderHeader on c.CustomerId equals s.CustomerId
+                                  join sd in db.SalesOrderDetail on s.SalesOrderId equals sd.SalesOrderId 
+                                  orderby s.CustomerId descending
+                                  select new
+                                  {
+                                      sd.SalesOrderDetailId,
+                                      sd.OrderQty,
+                                      sd.ProductId,
+                                      sd.UnitPrice,
+                                      sd.UnitPriceDiscount,
+                                      sd.LineTotal,
+                                      s.SalesOrderId,
+                                      s.RevisionNumber,
+                                      s.OrderDate,
+                                      s.DueDate,
+                                      s.ShipDate,
+                                      s.Status,
+                                      s.OnlineOrderFlag,
+                                      s.SalesOrderNumber,
+                                      s.PurchaseOrderNumber,
+                                      s.AccountNumber,
+                                      s.CustomerId,
+                                      s.ShipToAddressId,
+                                      s.BillToAddressId,
+                                      s.ShipMethod,
+                                      s.CreditCardApprovalCode,
+                                      s.SubTotal,
+                                      s.TaxAmt,
+                                      s.Freight,
+                                      s.TotalDue,
+                                      c.Title,
+                                      c.FirstName,
+                                      c.MiddleName,
+                                      c.LastName,
+                                      c.CompanyName,
+                                      c.SalesPerson,
+                                      c.EmailAddress,
+                                      c.Phone,
+                                 
+                                  }).ToList();
+            var newItems = innerJoinQuery.Where(x => !model.Any(y => x.CustomerId == y.CustomerId));
+            foreach (var item in newItems)
+            {
+               
+                model.Add(new ReportAttributes()
+                    {
+                        SalesOrderId = item.SalesOrderDetailId,
+                        OrderQty = item.OrderQty,
+                        ProductId = item.ProductId,
+                        UnitPrice = item.UnitPrice,
+                        UnitPriceDiscount = item.UnitPriceDiscount,
+                        LineTotal = item.LineTotal,
+                        RevisionNumber = item.RevisionNumber,
+                        OrderDate = item.OrderDate,
+                        DueDate = item.DueDate,
+                        ShipDate = item.ShipDate,
+                        Status = item.Status,
+                        OnlineOrderFlag = item.OnlineOrderFlag,
+                        SalesOrderNumber = item.SalesOrderNumber,
+                        PurchaseOrderNumber = item.PurchaseOrderNumber,
+                        AccountNumber = item.AccountNumber,
+                        CustomerId = item.CustomerId,
+                        ShipToAddressId = item.ShipToAddressId,
+                        BillToAddressId = item.BillToAddressId,
+                        ShipMethod = item.ShipMethod,
+                        CreditCardApprovalCode = item.CreditCardApprovalCode,
+                        SubTotal = item.SubTotal,
+                        TaxAmt = item.TaxAmt,
+                        TotalDue = item.TotalDue,
+                        Title = item.Title,
+                        FirstName = item.FirstName,
+                        MiddleName = item.MiddleName,
+                        LastName = item.LastName,
+                        CompanyName = item.CompanyName,
+                        SalesPerson = item.SalesPerson,
+                        EmailAddress = item.EmailAddress,
+                        Phone = item.Phone,
+                        CustomerName = item.LastName + ", " + item.FirstName
+                    });
+                }
+            return View(model);
+
         }
     }
 }
---
+++ src/awc/Models/ReportAttributes.cs
@@ -0,0 +1,56 @@
+﻿using awc.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace awc.Models
+{
+    public class ReportAttributes
+    {
+        //SalesOrderHeader
+        public int SalesOrderId { get; set; }
+        public byte RevisionNumber { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime? ShipDate { get; set; }
+        public byte Status { get; set; }
+        public bool OnlineOrderFlag { get; set; }
+        public string SalesOrderNumber { get; set; }
+        public string PurchaseOrderNumber { get; set; }
+        public string AccountNumber { get; set; }
+        public int CustomerId { get; set; }
+        public int? ShipToAddressId { get; set; }
+        public int? BillToAddressId { get; set; }
+        public string ShipMethod { get; set; }
+        public string CreditCardApprovalCode { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal TaxAmt { get; set; }
+        public decimal Freight { get; set; }
+        public decimal TotalDue { get; set; }
+        public string Comment { get; set; }
+        public Guid Rowguid { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+       //SalesOrderDetail
+        public int SalesOrderDetailId { get; set; }
+        public short OrderQty { get; set; }
+        public int ProductId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal UnitPriceDiscount { get; set; }
+        public decimal LineTotal { get; set; }
+
+        //Customer
+        public bool NameStyle { get; set; }
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string CustomerName { get; set; }
+        public string Suffix { get; set; }
+        public string CompanyName { get; set; }
+        public string SalesPerson { get; set; }
+        public string EmailAddress { get; set; }
+        public string Phone { get; set; }
+    }
+}
